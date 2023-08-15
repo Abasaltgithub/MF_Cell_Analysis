a3ef2c3b21f88d06822a0935538c9a572d0f7179
Abasalt Bahrami <83898640+Abasaltgithub@users.noreply.github.com>
2023-08-15 20:08:18 -0300

    update

--- CellCountConfluencyApp.py
+++ CellCountConfluencyApp.py
@@ -3,17 +3,20 @@ import numpy as np
 import os
 import tkinter as tk
 from tkinter import filedialog
+from PIL import Image, ImageTk
 
 
 def detect_circles(image_path, dp, minDist, param1, param2, minRadius, maxRadius):
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     image_height, image_width = image.shape
     blurred = cv2.GaussianBlur(image, (9, 9), 2)
-    circles = cv2.HoughCircles(blurred, cv2.HOUGH_GRADIENT, dp=dp, minDist=minDist,
-                               param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius)
+    circles = cv2.HoughCircles(
+        blurred, cv2.HOUGH_GRADIENT, dp=dp, minDist=minDist, param1=param1, param2=param2, minRadius=minRadius, maxRadius=maxRadius
+    )
 
     total_image_area = image_height * image_width
     if circles is not None:
+        circles = np.uint16(np.around(circles))
         total_contour_area = 0
         for circle in circles[0, :]:
             center = (circle[0], circle[1])
@@ -24,15 +27,23 @@ def detect_circles(image_path, dp, minDist, param1, param2, minRadius, maxRadius
 
         percentage_covered = (total_contour_area / total_image_area) * 100
         result_label.config(
-            text=f"Confidence for {image_filename}: {percentage_covered:.2f}%")
+            text=f"Confidence for {os.path.basename(image_path)}: {percentage_covered:.2f}%")
     else:
-        result_label.config(text=f"No circles detected in {image_filename}.")
+        result_label.config(
+            text=f"No circles detected in {os.path.basename(image_path)}.")
+
+    # Display the processed image in the Tkinter app
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    img = Image.fromarray(image)
+    img = ImageTk.PhotoImage(img)
+    image_label.config(image=img)
+    image_label.image = img
 
 
 def browse_image():
     global image_filename
     image_filename = filedialog.askopenfilename()
-    image_path_label.config(text="Image: " + image_filename)
+    image_path_label.config(text="Image: " + os.path.basename(image_filename))
 
 
 def detect_button_callback():
@@ -47,9 +58,11 @@ def detect_button_callback():
                    param2, minRadius, maxRadius)
 
 
+# Create the main application window
 app = tk.Tk()
 app.title("Circle Detection App")
 
+# Create and pack the widgets
 browse_button = tk.Button(app, text="Browse Image", command=browse_image)
 browse_button.pack()
 
@@ -93,4 +106,46 @@ detect_button.pack()
 result_label = tk.Label(app, text="")
 result_label.pack()
 
+# Label to display the processed image
+image_label = tk.Label(app)
+image_label.pack()
+
+# Set default values
+default_dp = 1.0
+default_minDist = 10.0
+default_param1 = 15.0
+default_param2 = 15.0
+default_minRadius = 4
+default_maxRadius = 20
+
+# Set initial values for entry fields
+dp_entry.insert(0, default_dp)
+minDist_entry.insert(0, default_minDist)
+param1_entry.insert(0, default_param1)
+param2_entry.insert(0, default_param2)
+minRadius_entry.insert(0, default_minRadius)
+maxRadius_entry.insert(0, default_maxRadius)
+
+
+def reset_default_values():
+    dp_entry.delete(0, tk.END)
+    minDist_entry.delete(0, tk.END)
+    param1_entry.delete(0, tk.END)
+    param2_entry.delete(0, tk.END)
+    minRadius_entry.delete(0, tk.END)
+    maxRadius_entry.delete(0, tk.END)
+
+    dp_entry.insert(0, default_dp)
+    minDist_entry.insert(0, default_minDist)
+    param1_entry.insert(0, default_param1)
+    param2_entry.insert(0, default_param2)
+    minRadius_entry.insert(0, default_minRadius)
+    maxRadius_entry.insert(0, default_maxRadius)
+
+
+reset_button = tk.Button(app, text="Reset Defaults",
+                         command=reset_default_values)
+reset_button.pack()
+
+# Start the main event loop
 app.mainloop()
